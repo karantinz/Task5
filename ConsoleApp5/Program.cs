1b60bd45d3827441ab258dee81dcf4551c0b6ae1
Danila <karantinz@mail.ru>
2021-09-09 15:28:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people;
+            int receptionHuman = 10;
+            int total;
+            int hours;
+            int minutes;
+
+            Console.WriteLine("Вы заходите в поликлинику и видите огромную очередь из старушек");
+            Console.WriteLine("Введите сколько их");
+            people = Convert.ToInt32(Console.ReadLine());
+
+            total = people * receptionHuman;
+            hours = total / 60;
+            minutes = total - (hours * 60);
+
+            Console.WriteLine("Вам нужно отстоять в очереди - " + hours + " часов " + minutes + " минут");
+            Console.ReadKey();
+        }
+    }
+}
